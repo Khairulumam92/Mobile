b7c45a9714bb97099849e45024cc7a619ab1cd7d
khairulumam92 <khairulumamku92@gmail.com>
2025-01-18 19:38:10 +0700

    update

--- Flutter/dart_application_1/bin/dart_application_1.dart
+++ Flutter/dart_application_1/bin/dart_application_1.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
 import 'package:dart_application_1/dart_application_1.dart'
     as dart_application_1;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
   print('Hello world2: ${dart_application_1.calculate()}!');
+
+  // New input functionality
+  stdout.write('Enter your name: ');
+  String? name = stdin.readLineSync();
+  print('Hello, $name!');
 }
